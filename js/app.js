5ddcb901df8868c6e8bff4b6e77ef1445c454615
Matt Katz <mrmattkatz@gmail.com>
2015-12-17 11:40:02 -0600

    Modal added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
-$('#send').click(function() {
-    var name = $('#inputName').val();
-    var email = $('#inputEmail').val();
-    var body = $('#inputBody').val();
+$('#sendRequest').click(function() {
+    var name = $('#name').val();
+    var email = $('#contact').val();
+    var brand = $('#brand').val();
+    var size = $('#size').val();
+    var quantity = $('#quantity').val();
+    var body = brand.concat(size, quantity);
     $.ajax({
         type: 'POST',
         url: 'https://mandrillapp.com/api/1.0/messages/send.json',
@@ -17,7 +20,35 @@ $('#send').click(function() {
                     }
                 ],
                 'autotext': 'true',
-                'subject': 'Test 4',
+                'subject': 'Product Request',
+                'text': body
+            }
+        }
+    }).done(function(response) {
+            alert('Your message was sent. Thank you!');
+    });
+});
+
+$('#sendProblem').click(function() {
+    var name = $('#name').val();
+    var email = $('#contact').val();
+    var body = $('#problem').val();
+    $.ajax({
+        type: 'POST',
+        url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+        data: {
+            'key': 'cC3lQUM2VNUZi1Lr1lCNmQ',
+            'message': {
+                'from_email': email,
+                'to': [
+                    {
+                        'email': 'mrmattkatz@gmail.com',
+                        'name': name,
+                        'type': 'to'
+                    }
+                ],
+                'autotext': 'true',
+                'subject': 'Problem/Suggestion',
                 'text': body
             }
         }
