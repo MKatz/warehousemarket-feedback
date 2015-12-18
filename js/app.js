340344c460e0dd294ae14d26c7d136c90044891b
Matt Katz <mrmattkatz@gmail.com>
2015-12-18 14:13:49 -0600

    Anon added, String added

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@ $('#sendRequest').click(function() {
     var brand = $('#brand').val();
     var size = $('#size').val();
     var quantity = $('#quantity').val();
-    var body = brand.concat(size, quantity);
+    // var body = brand.concat(size, quantity);
+    var body = "Brand - " + String(brand) + " Size - " + String(size) + " Quantity - " + String(quantity);
     $.ajax({
         type: 'POST',
         url: 'https://mandrillapp.com/api/1.0/messages/send.json',
@@ -25,13 +26,13 @@ $('#sendRequest').click(function() {
             }
         }
     }).done(function(response) {
-            alert('Your message was sent. Thank you!');
+            alert('Your product request was sent. Thank you!');
     });
 });
 
 $('#sendProblem').click(function() {
-    var name = $('#name').val();
-    var email = $('#contact').val();
+    var name = $('#nameProblem').val();
+    var email = $('#contactProblem').val();
     var body = $('#problem').val();
     $.ajax({
         type: 'POST',
@@ -53,6 +54,6 @@ $('#sendProblem').click(function() {
             }
         }
     }).done(function(response) {
-            alert('Your message was sent. Thank you!');
+            alert('Your problem/suggestion was sent. Thank you!');
     });
 });
